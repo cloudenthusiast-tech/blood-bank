dd433c5359c409711969c81ef77a7cb4b18b54b4
Aravind <kolanuaravindreddy@gmail.com>
2026-04-03 19:30:56 +0530

    blood-bank backend dockerfile

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173", // or 3000
+  origin: "http://bloodbank.kolanu.space", // or 3000
   credentials: true,
 }));
 
